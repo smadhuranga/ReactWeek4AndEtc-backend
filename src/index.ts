3554e0f726ced525f120a0b623f4f0475285483a
A.M. Supun Madhuranga <157113777+smadhuranga@users.noreply.github.com>
2025-06-28 15:03:04 +0530

    .

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,16 @@ import express, {Express , Request , Response} from "express";
 
 // 1. initialization the express app
 let app : Express = express();
-// Define port
+// 2.Define port
 const port = 3000;
 
-// 2. listen to port
+// 3. Define a simple HTTP get request
 app.get('/', (req : Request, res : Response) => {
   res.send("Hello World!");
+});
+
+// 4. start the server (Instructs the app to listen on the specified port)
+//listen on port 3000
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
