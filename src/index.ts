985fe524ea31c81de184fb38976c459f2ab7c0e3
A.M. Supun Madhuranga <157113777+smadhuranga@users.noreply.github.com>
2025-06-28 14:52:43 +0530

    file creation complete

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * @author supunmadhuranga
+ * @created 2025-06-28
+ * @project computer-shop-backend
+ * @description
+ */
+
+import express from "express";
+
+// 1. initialization the express app
+const app = express();
+// Define port
+const port = 3000;
+
+// 2. listen to port
+app.get('/', (req, res));
